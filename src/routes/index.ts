05c8503834dd4d4a52cb399f9a0ddac259faf027
khushboodaryani <khushboodaryani1@gmail.com>
2025-12-25 23:06:20 +0530

    updated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import userRoutes from "../public/user/user.route";
+import admin from "../admin/admin/admin.routes";
+import role from "../admin/role/role.routes";
 
 
 const router = Router();
 router.use("/user", userRoutes);
+router.use("/admin",admin);
+router.use("/role",role);
 
 
 export default router;
