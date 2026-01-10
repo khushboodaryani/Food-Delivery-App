dc7e6e8e8f3e0f829c48ecfab396400236110850
khushboodaryani <khushboodaryani1@gmail.com>
2026-01-10 23:48:27 +0530

    updated

---
+++ src/admin/category/category.route.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { CategoryController } from "./category.controller";
+import { asyncHandler } from "../../utils/asyncHandler";
+
+const router = Router();
+
